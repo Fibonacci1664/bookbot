168f38975b2b94281fbd368d181a26e02af993eb
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2025-04-29 09:46:18 +0100

    * Added new function to to stats.py to sort the dictionary of chars.
    *  Added new function to main.py to print a report of the analysis carried out on a given book.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_book_word_count, get_book_character_count
+from stats import get_book_word_count, get_book_character_count,sort_dict
 
 def get_book_text(file_path) -> str:
 
@@ -9,13 +9,27 @@ def get_book_text(file_path) -> str:
     
     return book_text
 
+def print_report(path, word_count, sorted_list) -> str:
+
+    print(f"============ BOOKBOT ===========\n" \
+          f"Analysing book found at {path}...\n" \
+          f"----------- Word Count -----------\n" \
+          f"Found {word_count} total words\n" \
+          f"--------- Character Count ---------")
+    
+    for item in sorted_list:
+        print(f"{item["char"]}: {item["num"]}")
+
+    print("============ END ===========")
+
 def main():
-    text = get_book_text("books/frankenstein.txt")  
+    file_path = "books/frankenstein.txt"
+    text = get_book_text(file_path)  
     char_count = get_book_character_count(text)
     word_count = get_book_word_count(text)
 
-    print(f"{word_count} words found in the document")
-    print(char_count)
+    sorted_list = sort_dict(char_count)
+    print_report(file_path, word_count, sorted_list)
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -17,3 +17,20 @@ def get_book_character_count(text) -> dict[str, int]:
             char_count[char] = 1 
 
     return char_count
+
+def sort_dict(dictionary) ->list[dict[str:int]]:
+    dict_list = []
+    
+    #sort by value, use a lamda to get the second value i.e. [1] from a tuple
+
+    # List of tuples
+    dict_view = dictionary.items()
+
+    sorted_list = sorted(dict_view, key=lambda tup: tup[1], reverse=True)
+
+    for tup in sorted_list:
+        if tup[0].isalpha():
+            temp_dict = {"char": tup[0], "num": tup[1]}
+            dict_list.append(temp_dict)
+
+    return dict_list
