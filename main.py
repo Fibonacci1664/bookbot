97e3fcd4d017d2b83a48ef7c0118b58c8e6c7459
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2025-05-15 09:00:51 +0100

    * Imported sys, so that the user can pass paths to books via the CLI.
    * Updated the code to use sys.argv

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import sys
 from stats import get_book_word_count, get_book_character_count,sort_dict
+
+
 
 def get_book_text(file_path) -> str:
 
@@ -23,7 +26,11 @@ def print_report(path, word_count, sorted_list) -> str:
     print("============ END ===========")
 
 def main():
-    file_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
     text = get_book_text(file_path)  
     char_count = get_book_character_count(text)
     word_count = get_book_word_count(text)
